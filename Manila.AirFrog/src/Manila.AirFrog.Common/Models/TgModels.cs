dd17e4ac896ce90f38e69433e6334f338a6fad1d
Lams Lupin <lams2135@gmail.com>
2016-11-21 03:14:12 +0800

    fix bugs

--- Manila.AirFrog/src/Manila.AirFrog.Common/Models/TgModels.cs
+++ Manila.AirFrog/src/Manila.AirFrog.Common/Models/TgModels.cs
@@ -5,10 +5,14 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Runtime.Serialization;
 
+    [DataContract]
     public class TgChatModel
     {
+        [DataMember(Name = "displayname")]
         public string DisplayName;
+        [DataMember(Name = "text")]
         public string Text;
     }
 }
